b5972263b7fa4e8562f16c3b834025f2bcc25c74
kazi-tawhid-ibn-monir <kazitawhidibnmonir@gmail.com>
2025-11-29 17:08:30 +0600

    navbar update

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -9,10 +9,10 @@
 
     <ul class="nav-links" id="navLinks">
         <li><a href="{{ route('home') }}" class="{{ Route::currentRouteName() === 'home' ? 'active' : '' }}">Home</a></li>
+        <li><a href="{{ route('about') }}" class="{{ Route::currentRouteName() === 'about' ? 'active' : '' }}">About</a></li>
         <li><a href="{{ route('skills') }}" class="{{ Route::currentRouteName() === 'skills' ? 'active' : '' }}">Skills</a></li>
         <li><a href="{{ route('projects') }}" class="{{ Route::currentRouteName() === 'projects' ? 'active' : '' }}">Projects</a></li>
-        <li><a href="{{ route('academic') }}" class="{{ Route::currentRouteName() === 'academic' ? 'active' : '' }}">Academic</a></li>
         <li><a href="{{ route('achievements') }}" class="{{ Route::currentRouteName() === 'achievements' ? 'active' : '' }}">Achievements</a></li>
-        <li><a href="{{ route('about') }}" class="{{ Route::currentRouteName() === 'about' ? 'active' : '' }}">About</a></li>
+        <li><a href="{{ route('academic') }}" class="{{ Route::currentRouteName() === 'academic' ? 'active' : '' }}">Academic</a></li>
     </ul>
 </nav>
